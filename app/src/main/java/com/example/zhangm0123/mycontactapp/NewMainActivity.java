244656436fa11dff64e1ee9576c9dd4a7bcf4d7c
meilong1 <meilong.zhang@gmail.com>
2018-05-22 08:07:25 -0700

    MyContactApp

---
+++ app/src/main/java/com/example/zhangm0123/mycontactapp/NewMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhangm0123.mycontactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
