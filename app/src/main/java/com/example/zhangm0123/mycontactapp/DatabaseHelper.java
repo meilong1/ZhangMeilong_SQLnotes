fffb048c975e2d48033c00d5e0d0fc459e6766f3
meilong1 <meilong.zhang@gmail.com>
2018-05-25 09:22:48 -0700

    Added address and phone

--- app/src/main/java/com/example/zhangm0123/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/zhangm0123/mycontactapp/DatabaseHelper.java
@@ -2,27 +2,29 @@ package com.example.zhangm0123.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "Contact2018.db";
     public static final String TABLE_NAME = "Contact2018_table";
     public static final String ID = "ID";
     public static final String COLUMN_NAME_CONTACT = "contact";
     public static final String COLUMN_ADDRESS_CONTACT = "address";
     public static final String COLUMN_PHONE_CONTACT = "number";
-    public static final String TABLE_ADDRESS = "Contact2018_address";
-    public static final String TABLE_PHONE = "Contact2018_number";
+
 
 
     public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    COLUMN_NAME_CONTACT + " TEXT)";
+                    COLUMN_NAME_CONTACT + " TEXT, " +
+                    COLUMN_ADDRESS_CONTACT + " TEXT, " +
+                    COLUMN_PHONE_CONTACT + " TEXT)";
 
     public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -35,15 +37,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         Log.d("MyContactApp", "Databasehelper: creating Databasehelper");
         db.execSQL(SQL_CREATE_ENTRIES);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.d("MyContactApp", "Databasehelper: upgrading Databasehelper");
         db.execSQL(SQL_DELETE_ENTRIES);
+        db.delete(TABLE_NAME, null, null);
         onCreate(db);
+
     }
 
     public boolean insertData(String name, String address, String number){
@@ -54,8 +60,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValue.put(COLUMN_ADDRESS_CONTACT, address);
         contentValue.put(COLUMN_PHONE_CONTACT, number);
 
-
+        //db.replace(TABLE_NAME, null, contentValue);
         long result = db.insert(TABLE_NAME, null, contentValue);
+
         if(result == -1){
             Log.d("MyContactApp", "Databasehelper: Contact insert - FAILED");
             return false;
@@ -66,5 +73,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    public Cursor getAllData(){
+        Log.d("MyContactApp", "Databasehelper: calling getAllData method");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
 
 }
